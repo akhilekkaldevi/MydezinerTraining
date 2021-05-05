2734f6338c42fc79b8ba9e0c6d85a4d89d1d05cb
rajeshpn9 <82865454+rajeshpn9@users.noreply.github.com>
2021-05-05 12:06:32 +0530

    Added postman code as task-2(1).js

---
+++ Task-2(jQuery)/task-2(1).js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $.get("https://064ed185-4fbe-4feb-a4b4-46a56c620855.mock.pstmn.io/Emp", function(data) {
+                data = JSON.parse(data);
+                $("#main").append("<tr class='empData'><th> Id </th><th> Name </th><th> Designation </th><th> DOB </th><th> Action </th></tr>");
+                //console.log(data.length);
+                for(let i = 0; i < data.length; i++){
+                    $("#main").append("<tr class='empBData'><td>" + data[i].Id + "</td><td>" + data[i].Name + "</td><td>" + data[i].Designation + "</td><td>" + data[i].DOB + "</td><td><div class='arrow-down'></div></td></tr>");
+                    $("#main").append("<tr class='dataB'><td colspan='5'><p>" + data[i].Name + " is '22' years old and he has 3 years of experience and earns 55000/month</p></td></tr>");
+                   // console.log(data[i].Name);
+                }
+
+                $(".dataB").hide();
+
+                for (let i = 0; i < data.length; i++) {
+                    $(".arrow-down").eq(i).click(function () {
+                        $(this).parent().parent().next().slideToggle("fast");
+                    })
+                }
+    });
+                
+});
+
+
+
+
--- Task-2(jQuery)/task-2.js
+++ Task-2(jQuery)/task-2.js
@@ -1,25 +1,34 @@
 $(document).ready(function(){
-                var arr= [];
-                for(let i = 0; i <= 3; i++){
-                    arr[i] = [];
-                }
-                arr = [[1234,"Ajay","Software Developer","16/12/1999"],[1235,"Rajesh","Software Engineer","12/16/1999"],[1236,"Manisyam","Software Engineer","01/01/1999"],[1111,"Uday","Software Developer","01/01/1999"]];
-                $("#main").append("<tr class='empData'><th> Id </th><th> Name </th><th> Designation </th><th> DOB </th><th> Action </th></tr>");
-                for(let j = 0; j < arr.length; j++){
-                    $("#main").append("<tr class='empBData'>");
-                    for(let k = 0; k < arr.length; k++){
-                        $("#main").append("<td>"+arr[j][k]+"</td>");
-                    }
-                    $("#main").append("<td> <div class='arrow-down'></div> </td>");
-                    $("#main").append("</tr>");
-                    $("#main").append("<tr class='data'><td colspan='5'><p>" + arr[j][1] + " is '22' years old and he has 3 years of experience and earns 55000/month</p></td></tr>");
-                }
+    /*var arr= [];
+    for(let i = 0; i <= 3; i++){
+        arr[i] = [];
+    }*/
+    //arr = [[1234,"Ajay","Software Developer","16/12/1999"],[1235,"Rajesh","Software Engineer","12/16/1999"],[1236,"Manisyam","Software Engineer","01/01/1999"],[1111,"Uday","Software Developer","01/01/1999"]];
+    let arr = [{Id : "1234", Name : "Ajay", Designation : "Software Developer", DOB : "07/11/1999"},
+           {Id : "1235", Name : "Rajesh", Designation : "Software Engineer", DOB : "16/12/1999"},
+           {Id : "1236", Name : "Manisyam", Designation : "Software Engineer", DOB : "01/01/1999"},
+           {Id : "1111", Name : "Uday", Designation : "Software Developer", DOB : "01/01/1999"}
+          ]
+    $("#main").append("<tr class='empData'><th> Id </th><th> Name </th><th> Designation </th><th> DOB </th><th> Action </th></tr>");
+    for(let i = 0; i < arr.length; i++){
+        $("#main").append("<tr class='empBData'><td>" + arr[i].Id + "</td><td>" + arr[i].Name + "</td><td>" + arr[i].Designation + "</td><td>" + arr[i].DOB + "</td><td><div class='arrow-down'></div></td></tr>");
+        $("#main").append("<tr class='dataB'><td colspan='5'><p>" + arr[i].Name + " is '22' years old and he has 3 years of experience and earns 55000/month</p></td></tr>");
+    }
+    /*for(let j = 0; j < arr.length; j++){
+        $("#main").append("<tr class='empBData'>");
+        for(let k = 0; k < arr.length; k++){
+            $("#main").append("<td>"+arr[j][k]+"</td>");
+        }
+        $("#main").append("<td> <div class='arrow-down'></div> </td>");
+        $("#main").append("</tr>");*/
+        
+    
 
-                $(".data").hide();
+    $(".dataB").hide();
 
-                for (let i = 0; i < arr.length; i++) {
-                    $(".arrow-down").eq(i).click(function () {
-                        $(this).parent().next().slideToggle("fast");
-                    })
-                }
-            });
+    for (let i = 0; i < arr.length; i++) {
+        $(".arrow-down").eq(i).click(function () {
+            $(this).parent().parent().next().slideToggle("fast");
+        })
+    }
+});
