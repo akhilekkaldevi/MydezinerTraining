869930ad74805d189052704651723d78f14cf3e0
abhinay710 <abhinaynaidu57@gmail.com>
2021-05-19 14:24:12 +0530

    Added Corrections

--- Java Tasks/RectangleOverlap/Rectangle.java
+++ Java Tasks/RectangleOverlap/Rectangle.java
@@ -1,14 +1,36 @@
 package RectangleOverlap;
 
-
 public class Rectangle {
-	Coordinate leftTop;
 
-	Coordinate rightBottom;
+	Coordinate rectangle1LeftTop;
 
-	public Rectangle(Coordinate leftTop, Coordinate rightBottom) {
+	Coordinate rectangle1RightBottom;
+
+	Coordinate rectangle2LeftTop;
+
+	Coordinate rectangle2RightBottom;
+
+	
+	public Rectangle(Coordinate rectangle1LeftTop, Coordinate rectangle1RightBottom, Coordinate rectangle2LeftTop,
+			Coordinate rectangle2RightBottom) {
 		
-		this.leftTop = leftTop;
-		this.rightBottom = rightBottom;
+		this.rectangle1LeftTop = rectangle1LeftTop;
+		this.rectangle1RightBottom = rectangle1RightBottom;
+		this.rectangle2LeftTop = rectangle2LeftTop;
+		this.rectangle2RightBottom = rectangle2RightBottom;
 	}
+
+	public void checkRectangle(Coordinate rectangle1LeftTop, Coordinate rectangle1RightBottom,
+			Coordinate rectangle2LeftTop, Coordinate rectangle2RightBottom) {
+
+		
+		if (rectangle1RightBottom.x < rectangle2LeftTop.x || rectangle2RightBottom.x < rectangle1LeftTop.x)
+			System.out.println("Rectangles do not overlap");
+		else if (rectangle1RightBottom.y > rectangle2LeftTop.y || rectangle2RightBottom.y > rectangle1LeftTop.y)
+			System.out.println("Rectangles do not overlap");
+		else
+			System.out.println("Rectangles are overlapping");
+		
+	}
+
 }
--- Java Tasks/RectangleOverlap/RectangleApplication.java
+++ Java Tasks/RectangleOverlap/RectangleApplication.java
@@ -1,8 +1,10 @@
 package RectangleOverlap;
 
 public class RectangleApplication {
-public static void main(String[] args) {
-	RectangleOverlap r=new RectangleOverlap();
-	r.checkRectangle();
+	public static void main(String[] args) {
+		Rectangle r = new Rectangle(new Coordinate(0, 5), new Coordinate(7, 0), new Coordinate(6, 7),
+				new Coordinate(10, 6));
+		r.checkRectangle(r.rectangle1LeftTop,r.rectangle1RightBottom,r.rectangle2LeftTop,r.rectangle2RightBottom);
+	}
 }
-}
+
