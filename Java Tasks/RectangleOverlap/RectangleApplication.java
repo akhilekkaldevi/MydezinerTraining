737785e8174623dac6cfb686cea5b6f924303910
abhinay710 <abhinaynaidu57@gmail.com>
2021-06-16 18:13:09 +0530

    Spring boot task

--- Java Tasks/RectangleOverlap/RectangleApplication.java
+++
@@ -1,9 +0,0 @@
-package RectangleOverlap;
-
-public class RectangleApplication {
-	public static void main(String[] args) {
-		Rectangle rectangle1 = new Rectangle(new Coordinate(0, 5), new Coordinate(7, 0));
-		Rectangle rectangle2 = new Rectangle(new Coordinate(6, 7),new Coordinate(10, 6));
-		rectangle1.checkRectangle(rectangle2);
-	}
-}
--- Java Tasks/School/MainClass.java
+++
@@ -1,9 +0,0 @@
-package School;
-
-public class MainClass {
-	public static void main(String[] args) {
-		UserClass us = new UserClass();
-		us.runProgram();
-
-	}
-}
